22a51c550d1db94eef9863dea667c02b73751d78
dougfort <doug.fort@gmail.com>
2023-03-06 15:06:22 -0500

    Chapter 7
    gave up on book definition of dot-product-1-1, (and sum-1)
    just used dot-product from malt

---
+++ chapter-07/chapter-07.rkt
@@ -0,0 +1,132 @@
+#lang racket
+
+;; 
+
+(module+ test
+  (require rackunit))
+
+(require malt)
+
+(declare-hyper revs)
+(declare-hyper alpha)
+(declare-hyper batch-size)
+
+(define line
+  (λ (x)
+    (λ (θ)
+      (+ (* (ref θ 0) x) (ref θ 1)))))
+
+(define line-xs (tensor 2.0 1.0 4.0 3.0))
+(define line-ys (tensor 1.8 1.2 4.2 3.3))
+
+; using the malt dot-product, can't get dot-product-1-1 to work
+(define plane
+  (λ (t)
+    (λ (theta)
+      (+ (dot-product (ref theta 0) t) (ref theta 1)))))
+
+(define plane-xs
+  (tensor
+   (tensor 1.0 2.05)
+   (tensor 1.0 3.0)
+   (tensor 2.0 2.0)
+   (tensor 2.0 3.91)
+   (tensor 3.0 6.13)
+   (tensor 4.0 8.09)))
+
+(define plane-ys
+  (tensor 13.99
+          15.99
+          18.0
+          22.4
+          30.2
+          37.94))
+
+(define l2-loss
+  (λ (target)
+    (λ (xs ys)
+      (λ (theta)
+        (let ([pred-ys ((target xs) theta)])
+          (sum
+           (sqr
+            (- ys pred-ys))))))))
+
+(define lonely-i
+  (λ (theta)
+    (map (λ (p)
+           (list p))
+         theta)))
+
+(define lonely-d
+  (λ (big-theta)
+    (map (λ (p)
+           (ref p 0))
+         big-theta)))
+
+(define lonely-u
+  (λ (big-theta gs)
+    (map (λ (p g)
+           (list (- (ref p 0) (* alpha g))))
+         big-theta
+         gs)))
+
+(define revise
+  (λ (f revs theta)
+    (cond
+      [(zero? revs) theta]
+      [else (revise f (sub1 revs) (f theta))])))
+
+(define gradient-descent
+  (λ (inflate deflate update)
+    (λ (obj theta)
+      (let ([f (λ (big-theta)
+                 (update
+                  big-theta
+                  (gradient-of obj (deflate big-theta))))])
+        (deflate (revise f revs (inflate theta)))))))
+
+(define lonely-gradient-descent
+  (gradient-descent lonely-i lonely-d lonely-u))
+
+(define samples
+  (λ (n s)
+    (sampled n s (list))))
+
+(define sampled
+  (λ (n i a)
+    (cond
+      [(zero? i) a]
+      [else (sampled n (sub1 i) (cons (random n) a))])))
+
+(define sampling-obj
+  (λ (expectant xs ys)
+    (let ([n (tlen xs)])
+      (λ (theta)
+        (let ([b (samples n batch-size)])
+          ((expectant (trefs xs b) (trefs ys b)) theta))))))
+
+(define try-line
+  (λ (a-gradient-descent)
+    (with-hypers
+        ([revs 15000]
+         [alpha 0.001]
+         [batch-size 4])
+      (a-gradient-descent
+       (sampling-obj (l2-loss line) line-xs line-ys) (list 0.0 0.0)))))
+
+(module+ test
+  (test-begin
+   (try-line lonely-gradient-descent)))
+
+(define try-plane
+  (λ (a-gradient-descent)
+    (with-hypers
+        ([revs 15000]
+         [alpha 0.001]
+         [batch-size 4])
+      (a-gradient-descent
+       (sampling-obj (l2-loss plane) plane-xs plane-ys) (list (tensor 0.0 0.0) 0.0)))))
+
+(module+ test
+  (test-begin
+   (try-plane lonely-gradient-descent)))
